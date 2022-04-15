321ca86c2e1b090c7777b725a4c4e2909ad1772f
danknjoroge <dank8505@gmail.com>
2022-04-15 20:58:54 +0300

    added models

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=288)
+    location = models.CharField(max_length=288)
+    occupants = models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE )
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    id = models.AutoField(max_length=8)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+
+
+
+
+
+
