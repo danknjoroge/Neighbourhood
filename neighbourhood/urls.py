d1d53d97f65ee7b425f47e532cce51d47edc0272
danknjoroge <dank8505@gmail.com>
2022-04-15 15:32:45 +0300

    added admin settings

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('neighbour.urls')),
 
     path('accounts/register/',
-        RegistrationView.as_view(success_url='/profile/'),
+        RegistrationView.as_view(success_url='/home/'),
         name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
