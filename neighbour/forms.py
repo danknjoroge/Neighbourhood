d308dda0aed66abb42fbfb0a0df0bada3c88cb59
danknjoroge <dank8505@gmail.com>
2022-04-16 07:49:22 +0300

    added forms.py

---
+++ neighbour/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Neighbourhood
+
+
+class NeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ['admin']
--- neighbour/models.py
+++ neighbour/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighbourhood(models.Model):
-    name = models.CharField(max_length=288)
+    neighbourhood_name = models.CharField(max_length=288)
     location = models.CharField(max_length=288)
     occupants = models.IntegerField()
     admin = models.ForeignKey(User, on_delete=models.CASCADE )
