4fc8e86d416d7fef5b506e95e273c3c38241b3ca
danknjoroge <dank8505@gmail.com>
2022-04-16 20:51:19 +0300

    Added profile view

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
-from neighbour.models import Neighbourhood
-from .forms import NeighbourhoodForm
+from neighbour.models import Neighbourhood, UserProfile
+from .forms import NeighbourhoodForm, ProfileForm
 
 # Create your views here.
 def index(request):
@@ -23,6 +23,20 @@ def home(request):
     return render(request, 'home.html', { "form": form, "neighbour": neighbour})
 
 
+def profile(request):
+    current_user = request.userprofile
+    form = ProfileForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        if form.is_valid():
+            profile= form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+        profile= UserProfile.objects.all()
+
+    return render(request, 'profile.html', {'form': form, 'profile': profile})
 
 
 
